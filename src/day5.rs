0e1071a6988917f111c82596ed5e9afb2de56096
Alice Boxhall <aboxhall@chromium.org>
2018-12-05 18:28:21 +1100

    Some cleanup

--- src/day5.rs
+++ src/day5.rs
@@ -1,17 +1,19 @@
 fn reduce(units: &mut Vec<char>) {
   let mut accumulator = Vec::<char>::new();
   units.iter_mut().fold(&mut accumulator, |vec, unit| {
-    let last_unit = vec.pop();
-    if last_unit.is_some()
-      && unit.eq_ignore_ascii_case(&last_unit.unwrap())
-      && ((unit.is_uppercase() && last_unit.unwrap().is_lowercase())
-        || (unit.is_lowercase() && last_unit.unwrap().is_uppercase()))
+    let maybe_last_unit = vec.pop();
+    if maybe_last_unit.is_none() {
+      vec.push(*unit);
+      return vec;
+    }
+    let last_unit = maybe_last_unit.unwrap();
+    if unit.eq_ignore_ascii_case(&last_unit)
+      && ((unit.is_uppercase() && last_unit.is_lowercase())
+        || (unit.is_lowercase() && last_unit.is_uppercase()))
     {
       return vec;
     }
-    if last_unit.is_some() {
-      vec.push(last_unit.unwrap());
-    }
+    vec.push(last_unit);
     vec.push(*unit);
     return vec;
   });
@@ -20,11 +22,12 @@ fn reduce(units: &mut Vec<char>) {
 
 pub fn run1(filename: &String) {
   //let polymer = "dabAcCaCBAcCcaDA";
-  let polymer = std::fs::read_to_string(filename).expect("Failed to read input");
+  let mut polymer = std::fs::read_to_string(filename).expect("Failed to read input");
+  polymer = polymer.trim().to_string();
   let mut units: Vec<char> = polymer.chars().collect();
   reduce(&mut units);
   let s: String = units.iter().collect();
-  println!("after folding: {:?}, {} units", s, s.len());
+  println!("after folding: {} units", s.len());
 }
 
 pub fn run2(filename: &String) {
@@ -33,12 +36,11 @@ pub fn run2(filename: &String) {
   polymer = polymer.trim().to_string();
   let units: Vec<char> = polymer.chars().collect();
   let mut unit_types = units.clone();
-  unit_types.sort_unstable();
-  unit_types.dedup();
   unit_types = unit_types
     .into_iter()
     .map(|c| c.to_uppercase().last().unwrap())
     .collect();
+  unit_types.sort_unstable();
   unit_types.dedup();
   let mut best = '?';
   let mut shortest = std::usize::MAX;
